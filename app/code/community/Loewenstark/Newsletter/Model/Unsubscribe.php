c0763673d7acca79e0ac6c077d6e5747f722abd2
Mathis Klooss <mathis@klooss.net>
2012-12-10 11:36:30 +0100

    typofx, reactivate confirmation mail after double opt in register

--- app/code/community/Loewenstark/Newsletter/Model/Unsubscribe.php
+++ app/code/community/Loewenstark/Newsletter/Model/Unsubscribe.php
@@ -3,8 +3,6 @@
 class Loewenstark_Newsletter_Block_Unsubscribe
 extends Mage_Newsletter_Model_Subscriber
 {
-    /** @var bool $_sendConfirmationSuccessEmail check if email already send **/
-    private $_sendConfirmationSuccessEmail = true;
 
     /**
      * unsubscribes by email
@@ -22,37 +20,5 @@ extends Mage_Newsletter_Model_Subscriber
         $this->setCheckCode($this->getCode());
         $this->unsubscribe();
         return $this->getStatus();
-    }
-    
-    /**
-     * Confirms subscriber newsletter
-     *
-     * @param string $code
-     * @return boolean
-     */
-    public function confirm($code)
-    {
-        $parent = (bool) parent::confirm($code);
-        if( $parent ) {
-            $this->sendConfirmationSuccessEmail();
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Sends out confirmation success email
-     * 
-     * @see Mage_Newsletter_Model_Subscriber::sendConfirmationSuccessEmail
-     *
-     * @return Mage_Newsletter_Model_Subscriber
-     */
-    public function sendConfirmationSuccessEmail() {
-        // do not send two E-Mails, may Magento will be implements this line in methode self::confirm($code)
-        if( $this->_sendConfirmationSuccessEmail ) {
-            parent::sendConfirmationSuccessEmail();
-            $this->_sendConfirmationSuccessEmail = !$this->_sendConfirmationSuccessEmail;
-        }
-        return $this;
     }
 }
