31506865fe0bd894beb0d757bb31bd337b106876
Mathis Klooss <mathis@klooss.net>
2012-12-10 13:59:47 +0100

    added original method from magento 1.7 (Mage_Newsletter_Model_Subscriber::subscribe)

--- app/code/community/Loewenstark/Newsletter/Model/Subscriber.php
+++ app/code/community/Loewenstark/Newsletter/Model/Subscriber.php
@@ -7,6 +7,73 @@ extends Mage_Newsletter_Model_Subscriber
     protected $_sendConfirmationSuccessEmail = true;
     
     /**
+     * Subscribes by email
+     *
+     * @param string $email
+     * @throws Exception
+     * @return int
+     */
+    public function subscribe($email)
+    {
+        $this->loadByEmail($email);
+        $customerSession = Mage::getSingleton('customer/session');
+
+        if(!$this->getId()) {
+            $this->setSubscriberConfirmCode($this->randomSequence());
+        }
+
+        $isConfirmNeed   = (Mage::getStoreConfig(self::XML_PATH_CONFIRMATION_FLAG) == 1) ? true : false;
+        $isOwnSubscribes = false;
+        $ownerId = Mage::getModel('customer/customer')
+            ->setWebsiteId(Mage::app()->getStore()->getWebsiteId())
+            ->loadByEmail($email)
+            ->getId();
+        $isSubscribeOwnEmail = $customerSession->isLoggedIn() && $ownerId == $customerSession->getId();
+
+        if (!$this->getId() || $this->getStatus() == self::STATUS_UNSUBSCRIBED
+            || $this->getStatus() == self::STATUS_NOT_ACTIVE
+        ) {
+            if ($isConfirmNeed === true) {
+                // if user subscribes own login email - confirmation is not needed
+                $isOwnSubscribes = $isSubscribeOwnEmail;
+                if ($isOwnSubscribes == true){
+                    $this->setStatus(self::STATUS_SUBSCRIBED);
+                } else {
+                    $this->setStatus(self::STATUS_NOT_ACTIVE);
+                }
+            } else {
+                $this->setStatus(self::STATUS_SUBSCRIBED);
+            }
+            $this->setSubscriberEmail($email);
+        }
+
+        if ($isSubscribeOwnEmail) {
+            $this->setStoreId($customerSession->getCustomer()->getStoreId());
+            $this->setCustomerId($customerSession->getCustomerId());
+        } else {
+            $this->setStoreId(Mage::app()->getStore()->getId());
+            $this->setCustomerId(0);
+        }
+
+        $this->setIsStatusChanged(true);
+
+        try {
+            $this->save();
+            if ($isConfirmNeed === true
+                && $isOwnSubscribes === false
+            ) {
+                $this->sendConfirmationRequestEmail();
+            } else {
+                $this->sendConfirmationSuccessEmail();
+            }
+
+            return $this->getStatus();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+    
+    /**
      * Confirms subscriber newsletter
      *
      * @param string $code
